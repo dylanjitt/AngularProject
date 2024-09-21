59aefa4c9d9549230ebf4b9514f722e4a84e60a8
Dylan Jitton <dylanjitton2.0@gmail.com>
2024-09-20 20:14:43 -0400

    tarea 1 20 sep 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title:number=30;
+
+  constructor(){
+    console.log("subtract",this.subtract(8,4))
+  }
+
+  public sum(num1:number,num2:number):number{
+    return num1+num2;
+  }
+
+  public subtract(num1:number,num2:number):number{
+    return num1-num2;
+  }
+
 }
