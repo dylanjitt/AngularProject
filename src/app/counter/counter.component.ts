6d498dd7f88fc4076cf84b2e675ec11c35233eb8
Dylan Jitton <dylanjitton2.0@gmail.com>
2024-10-03 20:41:27 -0400

    3 oct 2024 earlyRead mixedReadWrite write read

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -10,10 +10,38 @@ import { Component, afterRender, afterNextRender} from '@angular/core';
 export class CounterComponent {
 
   sum:number=0
+
+  appBackground: string='red'
   constructor(){
 
-    afterRender(()=> {
-      console.log("AFTER RENDER ",this.sum)
+    // afterRender(()=> {
+    //   console.log("AFTER RENDER ",this.sum)
+    // })
+
+    afterRender({// all phases in AfterRender
+      earlyRead: () => {
+        const currentColor = this.appBackground
+        return "From earlyRead: "+currentColor
+      },
+      mixedReadWrite:(props) => {
+        console.log("From MixedReadWrite: "+props)
+        if(props.indexOf('red')>-1){
+          this.appBackground='green'
+        }else{
+          this.appBackground='red'
+        }
+        return "From mixedReadWrite: "+this.appBackground
+      },
+      write:(props)=> {
+        console.log("From write: "+props)
+        document.body.style.backgroundColor=this.appBackground
+        return "From write: "+this.appBackground
+      },
+      read:(props)=>{
+        console.log("From read: "+props)
+        const newBackground=this.appBackground
+        return "From Read: "+this.appBackground
+      }
     })
     
     afterNextRender(()=> {
